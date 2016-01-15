2f351c895ac8d316eaf3f6e60adae2269790130b
kaplone <dvd.brtrnd@gmail.com>
2016-01-15 12:03:11 +0100

    nouvelle icone

--- LequelFX_scan/src/lequelFX_scan/Main.java
+++ LequelFX_scan/src/lequelFX_scan/Main.java
@@ -5,6 +5,7 @@ import javafx.stage.Stage;
 import utils.JfxUtils;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 
 public class Main extends Application {
@@ -14,6 +15,10 @@ public class Main extends Application {
 			Scene scene = new Scene((Parent) JfxUtils.loadFxml("GUI_scan.fxml"), 1000, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			
+			primaryStage.getIcons().add(new Image(getClass().getResource("LequelFX_scan_01.png").toExternalForm()));
+			primaryStage.setTitle("LequelFX_scan");
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
